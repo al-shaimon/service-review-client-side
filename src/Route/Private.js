4f91ed186444dd440618fa9d44e5d3f64f2ef83d
al-shaimon <alshaimon968@gmail.com>
2022-11-09 16:27:45 +0600

    private route added

---
+++ src/Route/Private.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../component/Context/AuthProvider/AuthProvider';
+
+/* 
+1. only allow authenticated user to visit the route
+2. 
+3. Redirect user to the route they wanted to go before login
+*/
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading, setLoading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return setLoading(true);
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
+};
+
+export default PrivateRoute;
