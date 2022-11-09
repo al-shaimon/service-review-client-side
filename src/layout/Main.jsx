1be0014192f54bc69b5e19338914e8d21a9774d2
al-shaimon <alshaimon968@gmail.com>
2022-11-09 18:39:35 +0600

    layout added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../component/Footer';
+import Header from '../component/Header';
+
+const Main = () => {
+  return (
+    <div>
+      <Header className=""></Header>
+      <div className="min-h-screen">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
